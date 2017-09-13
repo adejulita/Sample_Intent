731309e4284a4f224180e8bf8c6f51e5da175e5e
Ade Julita <addecanc@gmail.com>
2017-09-13 18:40:01 +0700

    Initial commit

---
+++ app/src/main/java/com/ade/android/sample_intent/Sub1Activity.java
@@ -0,0 +1,19 @@
+package com.ade.android.sample_intent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adej on 9/13/2017.
+ */
+
+public class Sub1Activity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub1);
+
+    }
+}
